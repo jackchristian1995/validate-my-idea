98d4a366fe3838f64e66daa98705963e56285775
Jack Christian <ccontact@jackchristian.uk>
2025-01-30 16:21:38 +1100

    error handling page

---
+++ error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="w-full h-screen flex flex-col items-center justify-center">
+    <h1>
+      <strong>ERROR: {{ error.statusCode }}</strong>
+    </h1>
+    <p>
+      Something went wrong, try starting again from the homepage.
+    </p>
+    <a href="/" class="cta">Start again</a>
+  </div>
+</template>
+
+<script setup>
+// Props
+const props = defineProps({
+  error: {
+    type: Object
+  }
+});
+</script>
